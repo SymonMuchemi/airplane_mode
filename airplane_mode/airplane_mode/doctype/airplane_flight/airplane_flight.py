e43549744d5d77509dff22d5dcdede2723542a04
symonmuchemi <simonkenya846@gmail.com>
2026-08-23 15:38:35 +0300

    feat(web): add airplane flight web views
    Configure Airplane Flight for website publishing and implement list/detail templates with required flight information and navigation links.

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
@@ -1,10 +1,14 @@
 # Copyright (c) 2026, SymonMuchemi and contributors
 # For license information, please see license.txt
 
-# import frappe
-from frappe.model.document import Document
+import frappe
+from frappe.website.website_generator import WebsiteGenerator
 
 
-class AirplaneFlight(Document):
-	def before_submit(self):
-		self.status = "Completed"
+class AirplaneFlight(WebsiteGenerator):
+    def validate(self):
+        if not self.route:
+            self.route = "flights/" + frappe.scrub(self.name)
+
+    def before_submit(self):
+        self.status = "Completed"
