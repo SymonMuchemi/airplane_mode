2eda6541610a11d37f1049de66b2d60b7d4c16d9
symonmuchemi <simonkenya846@gmail.com>
2026-08-26 02:18:20 +0300

    feat: Record Payment button on Shop Rent Invoice
    Added a Record Payment button to submitted invoices with an outstanding balance.

--- airplane_mode/airport_shop_management/doctype/shop_rent_invoice/shop_rent_invoice.js
+++ airplane_mode/airport_shop_management/doctype/shop_rent_invoice/shop_rent_invoice.js
@@ -1,8 +1,16 @@
 // Copyright (c) 2026, SymonMuchemi and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Shop Rent Invoice", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Shop Rent Invoice", {
+	refresh(frm) {
+		if (frm.doc.docstatus === 1 && flt(frm.doc.outstanding_amount) > 0) {
+			frm.add_custom_button(__("Record Payment"), () => {
+				frappe.new_doc("Shop Rent Payment", {
+					rent_invoice: frm.doc.name,
+					payment_date: frappe.datetime.get_today(),
+					amount_paid: frm.doc.outstanding_amount,
+				});
+			});
+		}
+	},
+});
