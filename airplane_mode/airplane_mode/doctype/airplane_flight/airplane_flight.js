aee9478c764e36a09ea490a0bacf08e5be33bc94
symonmuchemi <simonkenya846@gmail.com>
2026-08-25 22:39:17 +0300

    feat: add track gate to airplane flight and ticket doctypes

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.js
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.js
@@ -1,8 +1,31 @@
 // Copyright (c) 2026, SymonMuchemi and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airplane Flight", {
-// 	refresh(frm) {
+frappe.ui.form.on("Airplane Flight", {
+	refresh(frm) {
+		frm.add_custom_button(
+			__("Assign Gate"),
+			function () {
+				const dialog = new frappe.ui.Dialog({
+					title: __("Select Gate"),
+					fields: [
+						{
+							label: "Gate Number",
+							fieldname: "gate",
+							fieldtype: "Data",
+						},
+					],
+					size: "small",
+					primary_action_label: "Assign",
+					primary_action(values) {
+						frm.set_value("gate", values.gate);
+						dialog.hide();
+					},
+				});
 
-// 	},
-// });
+				dialog.show();
+			},
+			__("Actions")
+		);
+	},
+});
--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
@@ -2,6 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
+
 from frappe.website.website_generator import WebsiteGenerator
 
 
@@ -12,3 +13,30 @@ class AirplaneFlight(WebsiteGenerator):
 
     def before_submit(self):
         self.status = "Completed"
+
+    def on_update_after_submit(self):
+        self._enqueue_gate_sync()
+
+    def on_update(self):
+        self._enqueue_gate_sync()
+
+    def _enqueue_gate_sync(self):
+        old_doc = self.get_doc_before_save()
+
+        if not old_doc or not self.has_value_changed("gate"):
+            return
+
+        frappe.enqueue(
+            sync_gate_to_tickets,
+            queue="short",
+            enqueue_after_commit=True,
+            flight_name=self.name,
+        )
+
+
+def sync_gate_to_tickets(flight_name):
+    current_gate = frappe.db.get_value("Airplane Flight", flight_name, "gate")
+
+    frappe.db.set_value(
+        "Airplane Ticket", {"flight": flight_name}, "gate", current_gate
+    )
