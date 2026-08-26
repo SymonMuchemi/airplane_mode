ec1c6a1fcb1ace1beca4bacf326daff7e6abb204
symonmuchemi <simonkenya846@gmail.com>
2026-08-26 21:37:06 +0300

    feat: refine airport shop management configuration

--- airplane_mode/airport_shop_management/doctype/airport_shop/airport_shop.js
+++ airplane_mode/airport_shop_management/doctype/airport_shop/airport_shop.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2026, SymonMuchemi and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airport Shop", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Airport Shop", {
+	setup(frm) {
+		frm.set_query("type", () => {
+			return {
+				filters: {
+					is_enabled: 1,
+				},
+			};
+		});
+	},
+});
